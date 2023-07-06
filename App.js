e8ef1fc89751b6947764345c4558d147fe425b83
andrew <andrewasmit@hotmail.com>
2023-07-06 12:31:10 -0500

    expo maps installed with intial location specified and location coordinates saved in state after permissions granted

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
+import { useCallback, useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps'
+import * as Location from 'expo-location'
 
 export default function App() {
+  const [location, setLocation] = useState();
+
+  useEffect(()=>{
+    const getPermissions = async ()=>{
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted'){
+        console.log("Please grant permission")
+        return
+      } 
+      
+      let currentLocation = await Location.getCurrentPositionAsync();
+      setLocation(currentLocation);
+      console.log("Location: ", currentLocation);
+    }
+    getPermissions();
+  }, []);
+
+  const onRegionChange = useCallback((region) =>{
+    console.log(region);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView 
+        style={styles.map}
+        onRegionChange={onRegionChange}
+        initialRegion={{
+          // Dallas
+          latitude: 32.8053592247793, 
+          latitudeDelta: 0.865790580988083, 
+          longitude: -96.79484700876957, 
+          longitudeDelta: 0.5265133564613365
+        }}
+      >
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +52,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map:{
+    width: '100%',
+    height: '100%'
+  }
 });
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # map-app
+
+Practice app to incorporate Google Maps API and other 3rd party APIs
