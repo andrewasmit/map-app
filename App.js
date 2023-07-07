5cf60f8fed22ed912db7628399fffee3e3298e30
andrew <andrewasmit@hotmail.com>
2023-07-07 09:22:24 -0500

    marker and callout added although static

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { useCallback, useState, useEffect } from 'react';
 import { StyleSheet, Text, Button, TextInput, View } from 'react-native';
-import MapView from 'react-native-maps'
+import MapView, { Callout, Marker } from 'react-native-maps'
 import * as Location from 'expo-location'
 
 export default function App() {
   const [location, setLocation] = useState();
   const [address, setAddress] = useState();
 
+  
 
   useEffect(()=>{
     const getPermissions = async ()=>{
@@ -19,14 +20,20 @@ export default function App() {
       
       let currentLocation = await Location.getCurrentPositionAsync();
       setLocation(currentLocation);
-      console.log("Location: ", currentLocation);
+      // console.log("Location: ", currentLocation);
     }
     getPermissions();
   }, []);
 
+
+  if(location){
+    console.log("LOCATION :", location)
+  }
+
   const onRegionChange = useCallback((region) =>{
-    console.log(region);
+    console.log("CHANGED REGION: ", region);
   }, []);
+
 
   const handleAddressInput = useCallback((e)=>{
     setAddress(e)
@@ -53,6 +60,16 @@ export default function App() {
           longitudeDelta: 0.5265133564613365
         }}
       >
+        <Marker 
+          coordinate={{
+            latitude: 32.8053592247793, 
+            longitude: -96.79484700876957
+          }} 
+        >
+          <Callout>
+            <Text>I am here.</Text>
+          </Callout>
+        </Marker>
       </MapView>
       <StatusBar style="auto" />
     </View>
@@ -71,3 +88,17 @@ const styles = StyleSheet.create({
     height: '80%'
   }
 });
+
+
+// Location = {
+//   "coords": {
+//     "accuracy": 8.933091382932549, 
+//     "altitude": 197.318115234375, 
+//     "altitudeAccuracy": 4, 
+//     "heading": 275.9765625, 
+//     "latitude": 32.92318250055697, 
+//     "longitude": -96.6960574408031, 
+//     "speed": 0.2199999988079071
+//   }, 
+//   "timestamp": 1688738400999.975
+// }
