2302ae4e10211e5d29264bd80c7c37461dd8f478
andrew <andrewasmit@hotmail.com>
2023-07-07 10:06:26 -0500

    GooglePlaces API incorporated. Currently no key saved since we need to incorporate the env files for security purposes.

--- App.js
+++ App.js
@@ -1,76 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import { useCallback, useState, useEffect } from 'react';
-import { StyleSheet, Text, Button, TextInput, View } from 'react-native';
-import MapView, { Callout, Marker } from 'react-native-maps'
-import * as Location from 'expo-location'
+import { StatusBar } from "expo-status-bar";
+import { useCallback, useState, useEffect } from "react";
+import { StyleSheet, Text, Button, TextInput, View } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
 
 export default function App() {
-  const [location, setLocation] = useState();
+  const [location, setLocation] = useState({
+    latitude: 32.8053592247793,
+    longitude: -96.79484700876957,
+  });
   const [address, setAddress] = useState();
 
-  
-
-  useEffect(()=>{
-    const getPermissions = async ()=>{
+  useEffect(() => {
+    const getPermissions = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted'){
-        console.log("Please grant permission")
-        return
-      } 
-      
+      if (status !== "granted") {
+        console.log("Please grant permission");
+        return;
+      }
+
       let currentLocation = await Location.getCurrentPositionAsync();
       setLocation({
         latitude: currentLocation.coords.latitude,
-        longitude: currentLocation.coords.longitude
+        longitude: currentLocation.coords.longitude,
       });
       console.log("Location AFTER PERMISSION: ", location);
-    }
+    };
     getPermissions();
   }, []);
 
+  // if(location){
+  //   console.log("LOCATION: ", location)
+  // }
 
-  if(location){
-    // console.log("LOCATION: ", location)
-  }
-
-  const onRegionChange = useCallback((region) =>{
+  const onRegionChange = useCallback((region) => {
     // console.log("CHANGED REGION: ", region);
   }, []);
 
+  const handleAddressInput = useCallback(
+    (e) => {
+      setAddress(e);
+    },
+    [address]
+  );
 
-  const handleAddressInput = useCallback((e)=>{
-    setAddress(e)
-  }, [address]);
-
-  const geocode = async ()=>{
+  const geocode = async () => {
     const geocodedLocation = await Location.geocodeAsync(address);
     console.log("Geocoded Address: ", geocodedLocation);
     setAddress("");
-  }
+  };
 
   return (
-    <View style={styles.container}>
-      <TextInput placeholder="Address" value={address} onChangeText={handleAddressInput} />
-      <Button title="Geocode Address" onPress={geocode} />
-      <MapView 
+    <View style={{marginTop: 50, flex:1}}>
+      {/* <TextInput placeholder="Address" value={address} onChangeText={handleAddressInput} />
+      <Button title="Geocode Address" onPress={geocode} /> */}
+
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, details);
+        }}
+        query={{
+          key: "",
+          language: "en",
+        }}
+        styles={{
+          container: { flex: 0, position: 'absolute', width: '100%', zIndex: 1 },
+          listView: { backgroundColor: 'white' }
+        }}
+      />
+
+      <MapView
         style={styles.map}
         onRegionChange={onRegionChange}
         initialRegion={{
           // Dallas
-          latitude: 32.8053592247793, 
-          latitudeDelta: 0.865790580988083, 
-          longitude: -96.79484700876957, 
-          longitudeDelta: 0.5265133564613365
+          latitude: 32.8053592247793,
+          latitudeDelta: 0.865790580988083,
+          longitude: -96.79484700876957,
+          longitudeDelta: 0.5265133564613365,
         }}
       >
-        <Marker 
+        <Marker
           coordinate={{
-            latitude: location.latitude ,
-            longitude: location.longitude
-          }} 
+            latitude: location.latitude,
+            longitude: location.longitude,
+          }}
           draggable={true}
-          onDragStart={(e)=>console.log("Drag start: ", e.nativeEvent.coordinates)}
-          onDragEnd={(e)=>console.log("Drag end: ", e.nativeEvent.coordinates)}
+          onDragStart={(e) =>
+            console.log("Drag start: ", e.nativeEvent.coordinate)
+          }
+          onDragEnd={(e) => console.log("Drag end: ", e.nativeEvent.coordinate)}
         >
           <Callout>
             <Text>I am here.</Text>
@@ -85,26 +108,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  map:{
-    width: '100%',
-    height: '80%'
-  }
+  map: {
+    width: "100%",
+    height: "100%",
+  },
 });
-
-
-// Location = {
-//   "coords": {
-//     "accuracy": 8.933091382932549, 
-//     "altitude": 197.318115234375, 
-//     "altitudeAccuracy": 4, 
-//     "heading": 275.9765625, 
-//     "latitude": 32.92318250055697, 
-//     "longitude": -96.6960574408031, 
-//     "speed": 0.2199999988079071
-//   }, 
-//   "timestamp": 1688738400999.975
-// }
